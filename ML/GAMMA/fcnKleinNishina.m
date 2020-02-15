81ca988de6e0327e9a251ef9b366c734671a04c1
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-02-15 11:05:19 -0800

    Update fcnKleinNishina.m

--- ML/GAMMA/fcnKleinNishina.m
+++ ML/GAMMA/fcnKleinNishina.m
@@ -6,24 +6,20 @@ re=1; a=1;
 
 n=2000; ev=linspace(0,10,n); X=zeros(n);
 for i=1:n
-    E1=ev(i);  % initial gamma enerrgy
+    E1 = ev(i);  % initial gamma enerrgy
     E2 = linspace(0,E1,n);  % gamma energy after scatter
+    theta=real( acos( me*(1./E1-1./E2)+1 ) );  % gamma scatter angle
     
-    theta=real( acos( me*(1./E1-1./E2)+1 ) );  j=theta==pi;
+    j=theta==pi;
     
     %syms E2 E1 me theta;  solve(1/E2 - 1/E1 - 1/me*(1-cos(theta)),E1)
-    
     %ratio = 1./(1 + (E1./me)*(1-cos(theta))); %E2./E1
-    
     
     %dsdTh = re^2/2 * (E2./E1).^2.*(E2./E1+E1./E2 - sin(theta).^2); %https://en.wikipedia.org/wiki/Klein?Nishina_formula
     %y2 = a^2*re^2*ratio.^2.*(ratio+1./ratio-1+cos(theta).^2)/2;
     %dsdT = dsdTh.*(2*pi./(E1-(E1-E2)).^2);
-    
-    
     dsdT = (re^2*pi*(E1./E2 + E2./E1 - sin(real(acos(me*(1./E1 - 1./E2) + 1))).^2))./E1.^2; %simplified version
 
-    
     dsdT(j)=0; theta(j)=0;
     %sca; plot(E1-E2,theta*r2d,'.'); sca; plot((E1-E2),dsdT,'.'); %plot(E2,y2,'.')
     
